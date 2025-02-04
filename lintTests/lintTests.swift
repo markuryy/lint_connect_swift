004c1f114c47fcb6ef862b0b543ad0e14dd1b62c
mark <mr.catborg@gmail.com>
2025-02-04 00:16:04 -0500

    Initial Commit

---
+++ lintTests/lintTests.swift
@@ -0,0 +1,17 @@
+//
+//  lintTests.swift
+//  lintTests
+//
+//  Created by Mark Ogra on 2/4/25.
+//
+
+import Testing
+@testable import lint
+
+struct lintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
